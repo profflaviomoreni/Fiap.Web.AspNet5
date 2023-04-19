b9f777a90e9c5ab982a378c1357002880201fe5b
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-04-18 21:19:21 -0300

    Primeiro controller

---
+++ Fiap.Web.AspNet5/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet5.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //return Content("Fiap Asp.Net Core Shift 2023");
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            //return Content("Novo Cliente");
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Novo(string nome, string sobrenome)
+        {
+            return View("Sucesso");
+        }
+
+
+    }
+}
